8f362d17c6ede0dc7e58b100be7f2851e3793e34
BK-pk <hirakokisho@gmail.com>
2026-09-09 01:45:18 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='shiny-home-v72';
+const CACHE_NAME='shiny-home-v73';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install', event => {
